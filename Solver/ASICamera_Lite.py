e5efa55be3e1883d05fb9a6d5e070e2d55e63f55
AstroKeith <77962754+AstroKeith@users.noreply.github.com>
2024-07-02 11:05:22 +0100

    Add files via upload

---
+++ Solver/ASICamera_Lite.py
@@ -0,0 +1,92 @@
+from pathlib import Path
+from shutil import copyfile
+from CameraInterface import CameraInterface
+import zwoasi as asi
+import Display_Lite
+
+
+class ASICamera(CameraInterface):
+    """The camera class for ASI cameras.  Implements the CameraInterface interface."""
+
+    def __init__(self, handpad: Display_Lite) -> None:
+        """Initializes the ASI camera
+
+        Parameters:
+        handpad (Display): The link to the handpad"""
+
+        self.home_path = str(Path.home())
+        self.handpad = handpad
+
+        # find a camera
+        asi.init("/lib/zwoasi/armv8/libASICamera2.so")
+        num_cameras = asi.get_num_cameras()
+        if num_cameras == 0:
+            self.handpad.display("Error:", " no camera found", "")
+            self.camType = "not found"
+            print("camera not found")
+        else:
+            self.camType = "ZWO"
+            cameras_found = asi.list_cameras()
+            camera_id = 0
+            self.initialize()
+            self.handpad.display("ZWO camera found", "", "")
+            print("ZWO camera found")
+
+    def initialize(self) -> None:
+        """Initializes the camera and set the needed control parameters"""
+        global camera
+        if self.camType == "not found":
+            return
+        camera = asi.Camera(0)
+        camera.set_control_value(
+            asi.ASI_BANDWIDTHOVERLOAD, camera.get_controls()["BandWidth"]["MinValue"]
+        )
+        camera.disable_dark_subtract()
+        camera.set_control_value(asi.ASI_WB_B, 99)
+        camera.set_control_value(asi.ASI_WB_R, 75)
+        camera.set_control_value(asi.ASI_GAMMA, 50)
+        camera.set_control_value(asi.ASI_BRIGHTNESS, 50)
+        camera.set_control_value(asi.ASI_FLIP, 0)
+        camera.set_image_type(asi.ASI_IMG_RAW8)
+
+    def capture(
+        self, exposure_time: float, gain: float, m13: bool, polaris: bool, destPath: str) -> None:
+        """Capture an image with the camera
+
+        Parameters:
+        exposure_time (float): The exposure time in seconds
+        gain (float): The gain
+        radec (str): The Ra and Dec
+        m13 (bool): True if the example image of M13 should be used
+        polaris (bool): True if the example image of Polaris should be used
+        destPath (str): path to folder to save images, depends on Ramdisk selection
+        """
+        if self.camType == "not found":
+            self.handpad.display("camera not found", "", "")
+            return
+
+        camera.set_control_value(asi.ASI_GAIN, gain)
+        camera.set_control_value(asi.ASI_EXPOSURE, exposure_time)  # microseconds
+
+        if m13 == True:
+            print(self.home_path + "/Solver/test.jpg", destPath+"capture.jpg")
+            copyfile(
+                self.home_path + "/Solver/test.jpg",
+                destPath+"capture.jpg",
+            )
+        elif polaris == True:
+            copyfile(
+                self.home_path + "/Solver/polaris.jpg",
+                destPath+"capture.jpg",
+            )
+            print("using Polaris")
+        else:
+            camera.capture(filename=destPath+"capture.jpg")
+        return
+
+    def get_cam_type(self) -> str:
+        """Return the type of the camera
+
+        Returns:
+        str: The type of the camera"""
+        return self.camType
---
+++ Solver/Display_Lite.py
@@ -0,0 +1,78 @@
+#import time
+import serial
+import os
+import sys
+from PIL import Image,ImageDraw,ImageFont
+import board
+import adafruit_adxl34x
+
+class Handpad:
+    """All methods to work with the handpad"""
+
+    def __init__(self, version: str) -> None:
+        """Initialize the Handpad class,
+
+        Parameters:
+        version (str): The version of the eFinder software
+        """
+        global Image
+        self.version = version
+        i2c = board.I2C()
+        self.tilt = adafruit_adxl34x.ADXL345(i2c)
+        libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'drive')
+        if os.path.exists(libdir):
+            sys.path.append(libdir)
+        from drive import SSD1305
+        self.disp = SSD1305.SSD1305()
+        self.disp.command(0x81)# set Contrast Control - 1st byte
+        self.disp.command(0xFF)# set Contrast Control - 2nd byte - value
+        self.disp.Init()
+        self.disp.clear()
+        self.width = self.disp.width
+        self.height = self.disp.height
+        self.image = Image.new('1', (self.width, self.height))
+        self.draw = ImageDraw.Draw(self.image)
+        self.font = ImageFont.truetype("/home/efinder/Solver/text.ttf",8)
+        self.draw.rectangle((0,0,self.width,self.height), outline=0, fill=0)
+        self.draw.text((1, 0),"ScopeDog eFinder", font=self.font, fill=255)
+        self.draw.text((1, 8),"", font=self.font, fill=255)
+        self.draw.text((1, 16),"",  font=self.font, fill=255)
+        self.disp.getbuffer(self.image)
+        self.disp.ShowImage()
+        self.USB_module = False
+        self.display("ScopeDog", "eFinder v" + self.version, "")
+
+    def bright(self,brightness):
+        self.disp.command(0x81)# set Contrast Control - 1st byte
+        self.disp.command(int(brightness))# set Contrast Control - 2nd byte - value
+
+    def display(self, line0: str, line1: str, line2: str) -> None:
+        """Display the three lines on the display
+
+        """
+        self.draw.rectangle((0,0,self.width,self.height), outline=0, fill=0)
+        self.draw.text((1, 0),line0, font=self.font, fill=255)
+        self.draw.text((1, 10),line1, font=self.font, fill=255)
+        self.draw.text((1, 20),line2,  font=self.font, fill=255)
+
+        if self.tilt.acceleration[1] < 0:
+            im = self.image.transpose(Image.ROTATE_180)
+        else:
+            im = self.image
+
+        self.disp.getbuffer(im)
+        self.disp.ShowImage()
+        
+    def get_box(self) -> serial.Serial:
+        """Returns the box variable
+
+        Returns:
+        serial.Serial: The box variable"""
+        return self.box
+
+    def is_USB_module(self) -> bool:
+        """Return true if the handbox is an OLED
+
+        Returns:
+        bool: True is the handbox is an OLED"""
+        return self.USB_module
